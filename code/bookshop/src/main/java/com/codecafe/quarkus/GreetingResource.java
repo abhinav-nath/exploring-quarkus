2ccfc22ddc26a4c9f5f8ef426781b722ca97c9ce
Abhinav Nath <abhinavnath@ymail.com>
2022-05-27 23:14:08 +0530

    Update server port

--- code/bookshop/src/main/java/com/codecafe/quarkus/GreetingResource.java
+++ code/bookshop/src/main/java/com/codecafe/quarkus/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello World";
     }
 
 }
