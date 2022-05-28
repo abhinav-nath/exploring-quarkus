dadca5a8928ca9393df57aadaa578f2c801bc2ae
Abhinav Nath <abhinavnath@ymail.com>
2022-05-28 13:47:39 +0530

    Add two more endpoints to BookResource

--- code/bookshop/src/main/java/com/codecafe/quarkus/BookResource.java
+++ code/bookshop/src/main/java/com/codecafe/quarkus/BookResource.java
@@ -1,14 +1,17 @@
 package com.codecafe.quarkus;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.codecafe.quarkus.model.Book;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 @Path("/api/books")
 @Produces(APPLICATION_JSON)
@@ -31,6 +34,22 @@ public class BookResource {
           .genre("Programming")
           .yearOfPublication(2001)
           .build());
+  }
+
+  @GET
+  @Path("/count")
+  @Produces(TEXT_PLAIN)
+  public int countAllBooks() {
+    return getAllBooks().size();
+  }
+
+  @GET
+  @Path("/{id}")
+  public Optional<Book> getBook(@PathParam("id") int id) {
+    return getAllBooks()
+      .stream()
+      .filter(b -> b.getId() == id)
+      .findFirst();
   }
 
 }
