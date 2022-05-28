4931202f717f191cff4bf756e7b650f81e10cb36
Abhinav Nath <abhinavnath@ymail.com>
2022-05-28 17:20:15 +0530

    Add unit tests

--- code/bookshop/src/test/java/com/codecafe/quarkus/resource/BookResourceTest.java
+++ code/bookshop/src/test/java/com/codecafe/quarkus/resource/BookResourceTest.java
@@ -1,20 +1,50 @@
 package com.codecafe.quarkus.resource;
 
+import javax.ws.rs.core.MediaType;
+
 import io.quarkus.test.junit.QuarkusTest;
 
+import org.apache.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 class BookResourceTest {
 
   @Test
-  void testGetAllBooks() {
+  void shouldGetAllBooks() {
     given()
-      .when().get("/api/books")
-      .then()
-      .statusCode(200);
+      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+    .when()
+      .get("/api/books")
+    .then()
+      .statusCode(200)
+      .body("size()", is(2));
+  }
+
+  @Test
+  void shouldCountAllBooks() {
+    given()
+      .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
+    .when()
+      .get("/api/books/count")
+    .then()
+      .statusCode(200)
+      .body(is("2"));
+  }
+
+  @Test
+  void shouldGetABook() {
+    given()
+      .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+    .when()
+      .get("/api/books/1")
+    .then()
+      .statusCode(200)
+      .body("title", is("Dark Matter"))
+      .body("author", is("Blake Crouch"));
   }
 
 }
