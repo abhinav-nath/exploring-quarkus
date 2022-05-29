0decc655d11241e06065728f7d8ba209de8ff2c8
Abhinav Nath <abhinavnath@ymail.com>
2022-05-29 13:57:10 +0530

    Create Number Microservice

---
+++ code/number/src/native-test/java/com/codecafe/quarkus/microservices/number/NumberResourceIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.codecafe.quarkus.microservices.number;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NumberResourceIntegrationTest extends NumberResourceTest {
+
+  // Execute the same tests but in native mode.
+
+}
