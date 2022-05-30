218d4d3304b12190ef6de3761c62967aeaeae520
Abhinav Nath <abhinavnath@ymail.com>
2022-05-30 22:52:48 +0530

    Add test for Number Resource

--- code/number/src/test/java/com/codecafe/quarkus/microservices/number/resource/NumberResourceTest.java
+++ code/number/src/test/java/com/codecafe/quarkus/microservices/number/resource/NumberResourceTest.java
@@ -5,18 +5,22 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
-public class NumberResourceTest {
+class NumberResourceTest {
 
   @Test
-  public void testHelloEndpoint() {
+  void testHelloEndpoint() {
     given()
-      .when().get("/hello")
+      .when().get("/api/numbers")
       .then()
       .statusCode(200)
-      .body(is("Hello RESTEasy"));
+      .body("isbn_13", startsWith("13-"))
+      .body("isbn_10", startsWith("10-"))
+      .body(not(hasItem("generatedAt")));
   }
 
 }
