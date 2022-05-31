9bd3e77de5099935c16e1b0be3bd67bcf839a53a
Abhinav Nath <abhinavnath@ymail.com>
2022-05-31 23:53:25 +0530

    Customize the JSON output

--- code/book/src/main/java/com/codecafe/quarkus/microservices/book/model/Book.java
+++ code/book/src/main/java/com/codecafe/quarkus/microservices/book/model/Book.java
@@ -2,6 +2,9 @@ package com.codecafe.quarkus.microservices.book.model;
 
 import java.time.Instant;
 
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,11 +18,16 @@ import lombok.ToString;
 @AllArgsConstructor
 public class Book {
 
+  @JsonbProperty("isbn_13")
   private String isbn13;
+
   private String title;
   private String author;
+
   private int yearOfPublication;
   private String genre;
+
+  @JsonbDateFormat("yyyy-MM-dd")
   private Instant createdAt;
 
 }
